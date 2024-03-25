7fc7499f8bdec1147992ebf9b7967477f0178e39
Psychopomp3012 <114600784+Psychopomp3012@users.noreply.github.com>
2024-03-26 00:57:11 +0530

    Add files via upload

---
+++ React_Projects/src/App.js
@@ -0,0 +1,13 @@
+import Accordian from "./components/project1/index";
+
+
+const App = () => {
+  return (
+    // Accordian 
+    <div className="App">
+      <Accordian/>
+    </div>
+  )
+};
+
+export default App;
